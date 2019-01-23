4a57817fd8963b387a4bb3e544fc38bd609cc164
Cedric Müller AKKA <loc_admin@CMB04745.akka.eu>
2019-01-23 15:44:42 +0100

    highlight current navbar 23.01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,16 @@ export class AppComponent implements OnInit {
 
   navbarOpen = false;
 
-  public highlight: number;
   public imageUrlArray: String[] = [];
 
   private navbar: HTMLElement;
+  private highlight: HTMLElement;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.highlight = 1;
+    this.highlight = document.getElementById('Startseite');
+    this.highlight.className += ' active';
     this.imageUrlArray.push('../assets/Mannschaftsbild-Slideshow.jpg');
     this.imageUrlArray.push('../assets/Spielfeld-Slideshow.jpg');
     this.imageUrlArray.push('../assets/Oktoberfest-Slideshow.jpg');
@@ -35,4 +36,25 @@ export class AppComponent implements OnInit {
       this.navbar.className = 'topnav';
     }
   }
+
+  SetHighlight(highlight: string) {
+    this.highlight.className = 'navEntry';
+    this.highlight = document.getElementById(highlight);
+    this.highlight.className += 'active';
+  }
+
+
+  /*  CODE FOR HIGHLIGHT
+
+
+
+  $(document).ready(function () {
+    $('#navbar a').on("click", function () {
+        $(this).parent().siblings().find("a").removeClass('active');
+        $(this).addClass('active');
+    });
+  });
+
+
+  */
 }
